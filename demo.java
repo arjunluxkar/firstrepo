6df5b1699ec7734c2f70a4ff77f662210d752f44
arjunluxkar <arjunluxkar024@gmail.com>
2022-06-15 05:06:13 +0000

    code merged

--- demo.java
+++ demo.java
@@ -3,6 +3,13 @@ class demo
 	public static void main(...)
 	{
 	 s.o.print(" hello world ");
-   	 s.o.print(" hello world ");
+	 s.o.print(" hello world ");
+	 s.o.print(" hello world ");
+	 s.o.print(" hello world1 ");
+	 s.o.print(" hello world2 ");
+   	 s.o.print(" hello world3 ");
+   	 s.o.print(" hello world4 ");
+   	 s.o.print(" hello world5 ");
+   	 s.o.print(" hello world6 ");
 	}
 }
